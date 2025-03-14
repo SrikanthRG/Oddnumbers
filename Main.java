aaf88b44dd7d4f4839e5ae7f0340771808e42d55
Srikanth <93461143+SrikanthRG@users.noreply.github.com>
2025-03-14 14:01:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,17 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-            if (i % 2 != 0) { // check if the number is odd
+            if (i % 2 != 0) { 
                 System.out.print(i + " ");
             }
         }
-
-        System.out.println("\nEven numbers:"); // print even numbers
-        for (int i = 0; i <= 100; i++) {
-            if (i % 2 == 0) { // check if the number is even
-                System.out.print(i + " ");
-            }
-        }
-
     }
 }
